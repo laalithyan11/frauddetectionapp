ac19956b6a705cd70bd802077fe3c4d687955aae
Laalithya Nanjundaswamy <laalithyanswamy@gmail.com>
2026-07-30 06:41:55 +0000

    include these files

---
+++ src/main/java/com/finance/frauddetection/FrauddetectionApplication.java
@@ -0,0 +1,14 @@
+package com.finance.frauddetection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrauddetectionApplication {
+
+	public static void main(String[] args) {
+
+        SpringApplication.run(FrauddetectionApplication.class, args);
+	}
+
+}
